726addcf3d8a85b2929c1f8292f5853bdba562c2
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2020-01-22 19:11:13 +0100

    add composite spy

---
+++ Spy/Common/CompositeSpy.swift
@@ -0,0 +1,37 @@
+//
+//  CompositeSpy.swift
+//  Spy
+//
+//  Created by Tomasz Lewandowski on 22/01/2020.
+//  Copyright © 2020 AppUnite Sp. z o.o. All rights reserved.
+//
+
+public class CompositeSpy<Level: PSpyLevel, Channel: PSpyChannel>: PSpy {
+    private var spies: [AnySpy<Level, Channel>] = []
+    
+    public func addSpy(spy: AnySpy<Level, Channel>) -> CompositeSpy<Level, Channel> {
+        spies.append(spy)
+        return self
+    }
+        
+    public func configure(spyOnLevels: Set<Level>) -> Self {
+        for spy in spies {
+            spy.configure(spyOnLevels: spyOnLevels)
+        }
+        return self
+    }
+    
+    public func configure(spyOnChannels: Set<Channel>) -> Self {
+        for spy in spies {
+            spy.configure(spyOnChannels: spyOnChannels)
+        }
+        return self
+    }
+    
+    public func log(level: Level, channel: Channel, message: PSpyable) -> Self {
+        for spy in spies {
+            spy.log(level: level, channel: channel, message: message)
+        }
+        return self
+    }
+}
