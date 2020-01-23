c9bb858910dd8085fb1a46c57eb8cc3a28e82dfd
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2020-01-23 02:04:46 +0100

    example - network spy

--- Example/NetworkSpy.swift
+++ Example/NetworkSpy.swift
@@ -9,11 +9,15 @@
 import Spy
 
 public final class NetworkSpy<Level: PSpyLevel, Channel: PSpyChannel>: PSpy {
+    private var configuration: SpyConfiguration<Level, Channel> = SpyConfiguration()
+    
     public func apply(configuration: SpyConfiguration<Level, Channel>) -> Self {
+        self.configuration = configuration
         return self
     }
     
     public func log(level: Level, channel: Channel, message: PSpyable) -> Self {
+        guard configuration.shouldLog(level: level, channel: channel) else { return self }
         print("ToDo: send it over network: \(message.spyMessage)")
         return self
     }
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -23,7 +23,17 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         Environment.spy.log(level: .info, channel: .lifecycle, message: "ViewDidLoad")
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        Environment.spy.log(level: .info, channel: .lifecycle, message: "ViewDidAppear")
+        Environment.spy.log(level: .severe, channel: .other, message: "ViewDidAppear not implemented")
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        Environment.spy.log(level: .warning, channel: .lifecycle, message: "ViewWillAppear")
     }
 
     override var representedObject: Any? {
