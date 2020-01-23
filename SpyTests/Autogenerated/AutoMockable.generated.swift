aef951b635531136713a043cd61b4a8fbe5f5d04
Tomasz Lewandowski <tomasz.t.lewandowski@gmail.com>
2020-01-23 23:02:55 +0100

    configure sourcery automocks

---
+++ SpyTests/Autogenerated/AutoMockable.generated.swift
@@ -0,0 +1,31 @@
+// Generated using Sourcery 0.17.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+// swiftlint:disable line_length
+// swiftlint:disable variable_name
+
+import Foundation
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX)
+import AppKit
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Copyright © 2020 AppUnite Sp. z o.o. All rights reserved.
+
+import Foundation
